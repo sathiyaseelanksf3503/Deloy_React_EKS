292144a3f67cbbf8c09b4a8299d26ecd070969d7
sathiyaseelanksf3503 <Sathiyaseelan.kannigaraj@syncfusion.com>
2024-05-09 22:18:39 +0530

    react_eks sample

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+         ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
+
+function App() {
+    return (<div>
+    <div className='control-section'>
+    {/* Render the PDF Viewer */}
+      <PdfViewerComponent
+        id="container"
+        documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
+        serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer" 
+        style={{ 'height': '640px' }}>
+
+         <Inject services={[ Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView,
+                             ThumbnailView, Print, TextSelection, TextSearch, FormFields, FormDesigner ]}/>
+      </PdfViewerComponent>
+    </div>
+  </div>);
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
